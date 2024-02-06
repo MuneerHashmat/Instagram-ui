da67f46edc08f835c2c5fdbccb1ba0fc25a43888
Muneer Hashmat <muneerhashmat@gmail.com>
2024-02-06 13:57:42 +0530

    added media queries to home page for width 1160px

--- muneer/index.js
+++ muneer/index.js
@@ -1,10 +1,15 @@
 // create post modal
 
 const modal = document.querySelector("#modal");
-const openModal = document.querySelector("#open-button");
+const openModal = document.querySelector(".open-button");
+const openModal2 = document.querySelector(".open-button2");
 const closeModal = document.querySelector(".close-button");
 
 openModal.addEventListener("click", () => {
+    modal.showModal();
+});
+
+openModal2.addEventListener("click", () => {
     modal.showModal();
 });
 
